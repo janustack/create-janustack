60a21031719f6fbe5f13659522e4504e4d05162b
andrewcwhy <acydeveloper05@gmail.com>
2025-06-25 22:40:36 -0400

    wip

--- crates/create/templates/janudocs/react/vite.config.ts
+++ crates/create/templates/janudocs/react/vite.config.ts
@@ -1,14 +1,14 @@
 import { resolve } from "node:path";
 
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindCSS from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
-		TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+		tanstackRouter({ target: "react", autoCodeSplitting: true }),
 		react(),
 		tailwindCSS(),
 	],
